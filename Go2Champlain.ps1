3a67b2f826a32c61c7423d451db03f7419ae6c75
Kevin <90580134+RigPig@users.noreply.github.com>
2024-01-26 12:02:54 -0500

    Delete Go2Champlain.ps1

--- Go2Champlain.ps1
+++
@@ -1,4 +0,0 @@
-﻿$chrome = Get-Process -name "chrome" -ErrorAction SilentlyContinue
-if ($chrome -eq $null) {
-Start-Process "chrome" -ArgumentList "https://www.champlain.edu" }
-else { Stop-Process -Name "chrome" -Force }
