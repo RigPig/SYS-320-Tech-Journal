7c1a9e1b4a0202897722771cdf08c840fcf50ba4
Kevin <90580134+RigPig@users.noreply.github.com>
2024-01-26 11:59:53 -0500

    Add files via upload
    If an instance of chrome is not running, it starts chrome and directs it to champlain.edu. If an instance is already running, it closes it.

---
+++ Go2Champlain.ps1
@@ -0,0 +1,4 @@
+﻿$chrome = Get-Process -name "chrome" -ErrorAction SilentlyContinue
+if ($chrome -eq $null) {
+Start-Process "chrome" -ArgumentList "https://www.champlain.edu" }
+else { Stop-Process -Name "chrome" -Force }
