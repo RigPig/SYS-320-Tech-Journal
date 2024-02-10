b9dd3cabc63467ca059fa8145463b30a346ecdc5
unknown <kevin.botsaris@mymail.champlain.edu>
2024-02-09 19:32:27 -0500

    This script calls the functions from my first script

---
+++ week3/Week3Script2.ps1
@@ -0,0 +1,24 @@
+﻿
+. (Join-Path $PSScriptRoot "Week3Script1.ps1")
+
+clear
+
+# Get login and logoff records from the last 15 days
+$daysToRetrieveLoginLogoff = 15
+$loginLogoffRecords = Get-LoginLogoffRecords -DaysAgo $daysToRetrieveLoginLogoff
+
+Write-Host "Login and Logoff Records from the last $daysToRetrieveLoginLogoff days:"
+$loginLogoffRecords | Format-Table -AutoSize
+
+# Get shutdown records from the last 25 days
+$daysToRetrieveShutdown = 25
+$shutdownRecords = Get-ComputerStartShutdownTimes -DaysAgo $daysToRetrieveShutdown | Where-Object { $_.Event -eq "Shutdown" }
+
+Write-Host "`nShutdown Records from the last $daysToRetrieveShutdown days:"
+$shutdownRecords | Format-Table -AutoSize
+
+# Get startup records from the last 25 days
+$startupRecords = Get-ComputerStartShutdownTimes -DaysAgo $daysToRetrieveShutdown | Where-Object { $_.Event -eq "Start" }
+
+Write-Host "`nStartup Records from the last $daysToRetrieveShutdown days:"
+$startupRecords | Format-Table -AutoSize
