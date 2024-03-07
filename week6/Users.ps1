324ce91df074fb637d7faf97d289f645dfce43d6
unknown <kevin.botsaris@mymail.champlain.edu>
2024-03-07 18:51:42 -0500

    Turn to Menu

---
+++ week6/Users.ps1
@@ -0,0 +1,129 @@
+﻿<# ******************************
+# Create a function that returns a list of NAMEs AND SIDs only for enabled users
+****************************** #>
+function getEnabledUsers(){
+
+  $enabledUsers = Get-LocalUser | Where-Object { $_.Enabled -ilike "True" } | Select-Object Name, SID
+  return $enabledUsers
+  
+}
+
+
+<# ******************************
+# Create a function that returns a list of NAMEs AND SIDs only for not enabled users
+****************************** #>
+function getNotEnabledUsers(){
+
+  $notEnabledUsers = Get-LocalUser | Where-Object { $_.Enabled -ilike "False" } | Select-Object Name, SID
+  return $notEnabledUsers
+
+}
+
+
+
+
+<# ******************************
+# Create a function that adds a user
+****************************** #>
+function createAUser($name, $password){
+
+   $params = @{
+     Name = $name
+     Password = $password
+   }
+
+   $newUser = New-LocalUser @params 
+
+
+   # ***** Policies ******
+
+   # User should be forced to change password
+   Set-LocalUser $newUser -PasswordNeverExpires $false
+
+   # First time created users should be disabled
+   Disable-LocalUser $newUser
+
+}
+
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+
+function removeAUser($name){
+   
+   $userToBeDeleted = Get-LocalUser | Where-Object { $_.name -ilike $name }
+   Remove-LocalUser $userToBeDeleted
+   
+}
+
+
+
+function disableAUser($name){
+   
+   $userToBeDeleted = Get-LocalUser | Where-Object { $_.name -ilike $name }
+   Disable-LocalUser $userToBeDeleted
+   
+}
+
+
+function enableAUser($name){
+   
+   $userToBeEnabled = Get-LocalUser | Where-Object { $_.name -ilike $name }
+   Enable-LocalUser $userToBeEnabled
+   
+}
+function checkUser($name){
+    $userExists = Get-LocalUser -Name $name -ErrorAction SilentlyContinue
+    if ($userExists -ne $null) {
+        return $true
+    } else {
+        return $false
+    }
+}
+
+
+function getFailedLogins($timeBack){
+  
+  $failedlogins = Get-EventLog security -After (Get-Date).AddDays("-"+"$timeBack") | Where { $_.InstanceID -eq "4625" }
+
+  $failedloginsTable = @()
+  for($i=0; $i -lt $failedlogins.Count; $i++){
+
+    $account=""
+    $domain="" 
+
+    $usrlines = getMatchingLines $failedlogins[$i].Message "*Account Name*"
+    $usr = $usrlines[1].Split(":")[1].trim()
+
+    $dmnlines = getMatchingLines $failedlogins[$i].Message "*Account Domain*"
+    $dmn = $dmnlines[1].Split(":")[1].trim()
+
+    $user = $dmn+"\"+$usr;
+
+    $failedloginsTable += [pscustomobject]@{"Time" = $failedlogins[$i].TimeGenerated; `
+                                       "Id" = $failedlogins[$i].InstanceId; `
+                                    "Event" = "Failed"; `
+                                     "User" = $user;
+                                     }
+
+    }
+    }
+   function getListAtRiskUsers($days){
+    $failedLogins = getFailedLogins $days
+    $atRiskUsers = $failedLogins | Group-Object -Property User | Where-Object { $_.Count -gt 10 } | Select-Object -ExpandProperty Name
+    return $atRiskUsers
+}
+
+
